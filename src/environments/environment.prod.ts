109aac023e442537cf1924fec5bc6aa6e0ec2fe1
chjoguer <cjguerrero_garcia@outlook.com>
2021-01-30 15:17:48 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   // apiUrl: "http://127.0.0.1:8000",
   // imgUrl: "http://127.0.0.1:8000",
- apiUrl: "http://chjoguer.pythonanywhere.com",
-  imgUrl: "http://chjoguer.pythonanywhere.com",
+ apiUrl: "https://chjoguer.pythonanywhere.com",
+  imgUrl: "https://chjoguer.pythonanywhere.com",
 
 
 };
